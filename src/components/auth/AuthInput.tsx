81c43a896c8f6db45ae2cddd88ee90c1d8f5c0c4
Leonardo Santos <leonardojosantos@gmail.com>
2023-08-15 15:55:04 -0300

    implementation of 'Authentication' component - second part

--- src/components/auth/AuthInput.tsx
+++ src/components/auth/AuthInput.tsx
@@ -5,16 +5,30 @@ export default function AuthInput(props: AuthInputProps) {
         props.doNotRenderWhen
             ? null
             : (
-                <div>
+                <div className='flex flex-col mt-4'>
                     <label>
-                        {props.label}
+                        {
+                            props.label
+                        }
                     </label>
 
                     <input
                         type={props.type ?? 'text'}
                         value={props.value}
                         onChange={e => props.changeValue?.(e.target.value)}
-                        required={props.required} />
+                        required={props.required}
+                        className={`
+                           px-4
+                           py-3
+                           rounded-lg
+                           bg-gray-200
+                           mt-2
+                           border
+                           focus:border-blue-500
+                           focus:bg-white
+                           focus:outline-none
+                        `}
+                    />
                 </div>
             )
     );
--- src/pages/authentication.tsx
+++ src/pages/authentication.tsx
@@ -1,28 +1,101 @@
 import { useState } from 'react';
 import AuthInput from '../components/auth/AuthInput';
+import { IconGoogle } from '../components/icons';
 
 export default function Authentication() {
     const [modo, setModo] = useState<'login' | 'register'>('login');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    function submit() {
+        if (modo === 'login') {
+            console.log('login');
+        } else {
+            console.log('cadastrar');
+        }
+    };
+
     return (
-        <div className='flex flex-col'>
-            <h1>Autenticação</h1>
+        <div className='
+            flex
+            flex-col
+            h-screen
+            items-center
+            justify-center'
+        >
 
-            <AuthInput
-                label='E-mail'
-                type='email'
-                value={email}
-                changeValue={setEmail}
-                required />
+            <div className='w-1/2'>
+                <h1 className={`
+                    text-xl
+                    font-bold
+                    mb-5
+                `}>
 
-            <AuthInput
-                label='Senha'
-                type='password'
-                value={password}
-                changeValue={setPassword}
-                required />
+                    {
+                        modo === 'login'
+                            ? 'Entre com a Sua Conta'
+                            : 'Cadastre-se na Plataforma'
+                    }
+                </h1>
+
+                <AuthInput
+                    label='E-mail'
+                    type='email'
+                    value={email}
+                    changeValue={setEmail}
+                    required
+                />
+
+                <AuthInput
+                    label='Senha'
+                    type='password'
+                    value={password}
+                    changeValue={setPassword}
+                    required
+                />
+
+                <button onClick={submit}
+                    className={`
+                        w-full
+                        bg-indigo-500
+                        hover:bg-indigo-400
+                        text-white
+                        rounded-lg
+                        px-4
+                        py-3
+                        mt-6
+                    `}>
+
+                    {
+                        modo === 'login' ? 'Entrar' : 'Cadastrar'
+                    }
+                </button>
+
+                <hr className={`
+                    my-6
+                    border-gray-300
+                    w-full
+                `} />
+
+                <button onClick={submit}
+                    className={`
+                        w-full
+                        bg-red-500
+                        hover:bg-red-400
+                        text-white
+                        rounded-lg
+                        px-4
+                        py-3
+                    `}
+                >
+
+                    {
+                        IconGoogle(3)
+                    }
+
+                    Entrar com Google
+                </button>
+            </div>
         </div>
     );
 };
