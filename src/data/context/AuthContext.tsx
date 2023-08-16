36e51dc7c9eb68b9faa92c3f5bdc23ae7bcea022
Leonardo Santos <leonardojosantos@gmail.com>
2023-08-16 13:45:07 -0300

    implemented authentication functionality with 'google'

--- src/data/context/AuthContext.tsx
+++ src/data/context/AuthContext.tsx
@@ -6,25 +6,32 @@ import User from '@/src/model/UserModel';
 
 const AuthContext = createContext<AuthContextProps>({});
 
-// async function normalizedUser(firebaseUser: firebase.User): Promise<User> {
-//     const token = await firebaseUser.getIdToken();
+async function normalizedUser(firebaseUser: firebase.User): Promise<User> {
+    const token = await firebaseUser.getIdToken();
 
-//     return {
-//         uid: firebaseUser.uid,
-//         name: firebaseUser.displayName,
-//         email: firebaseUser.email,
-//         token,
-//         provider: firebaseUser.providerData[0].providerId,
-//         imageUrl: firebaseUser.photoURL,
-//     };
-// };
+    return {
+        uid: firebaseUser.uid,
+        name: firebaseUser.displayName,
+        email: firebaseUser.email,
+        token,
+        provider: firebaseUser.providerData[0].providerId,
+        imageUrl: firebaseUser.photoURL,
+    };
+};
 
 export function AuthProvider(props: any) {
     const [user, setUser] = useState<User>(null);
 
     async function loginGoogle() {
-        console.log('Login Google...');
-        route.push('/');
+        const resp = await firebase.auth().signInWithPopup(
+            new firebase.auth.GoogleAuthProvider()
+        );
+
+        if (resp.user?.email) {
+            const user = await normalizedUser(resp.user);
+            setUser(user);
+            route.push('/');
+        }
     };
 
     return (
